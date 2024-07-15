016366d0dbd66f625985299f6af3ebd5b3984d36
Taoheedoh <taoheedoh@gmail.com>
2024-07-15 21:32:54 +0100

    Add: width

--- lib/main.dart
+++ lib/main.dart
@@ -24,28 +24,32 @@ class _MyAppState extends State<MyApp> {
           title: const Text('Flutter Basics'),
         ),
         body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              ElevatedButton(
-                onPressed: () {
-                  setState(() {
-                    buttonName = 'Clicked';
-                  });
-                },
-                child: Text(buttonName),
-              ),
-
-              ElevatedButton(
-                onPressed: () {
-                  setState(() {
-                    buttonName = 'Clicked';
-                  });
-                },
-                child: Text(buttonName),
-              ),
-
-            ],
+          child: SizedBox(
+            width: double.infinity,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      buttonName = 'Clicked';
+                    });
+                  },
+                  child: Text(buttonName),
+                ),
+            
+                ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      buttonName = 'Clicked';
+                    });
+                  },
+                  child: Text(buttonName),
+                ),
+            
+              ],
+            ),
           ),
         ),
         bottomNavigationBar: BottomNavigationBar(
