614d731dd11a44cb0005adb8bc17e5cef67ff813
Taoheedoh <taoheedoh@gmail.com>
2024-07-18 19:15:18 +0100

    Add: Gesture Dectector

--- lib/main.dart
+++ lib/main.dart
@@ -12,8 +12,6 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -31,81 +29,90 @@ class MyAppExt extends StatefulWidget {
 }
 
 class _MyAppExtState extends State<MyAppExt> {
-
   String buttonName = 'Click';
   int currentIndex = 0;
+  bool _isClicked = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Flutter Basics'),
-        ),
-        body: Center(
-          child: currentIndex == 0
-              ? Container(
-                  width: double.infinity,
-                  height: double.infinity,
-                  color: Colors.red,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          foregroundColor: Colors.white,
-                          backgroundColor: Colors.green,
-                        ),
-                        onPressed: () {
-                          setState(() {
-                            buttonName = 'Clicked';
-                          });
-                        },
-                        child: Text(buttonName),
+      appBar: AppBar(
+        title: const Text('Flutter Basics'),
+      ),
+      body: Center(
+        child: currentIndex == 0
+            ? Container(
+                width: double.infinity,
+                height: double.infinity,
+                color: Colors.red,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        foregroundColor: Colors.white,
+                        backgroundColor: Colors.green,
                       ),
-                      ElevatedButton(
-                        style: ElevatedButton.styleFrom(
-                          foregroundColor: Colors.orange,
-                          backgroundColor: Colors.black,
-                        ),
-                        onPressed: () {
-                          Navigator.of(context).push(
-                            MaterialPageRoute(
-                              builder: (context) =>
-                                  const NextPage(),
-                            ),
-                          );
-                        },
-                        child: const Text('Next Page'),
+                      onPressed: () {
+                        setState(() {
+                          buttonName = 'Clicked';
+                        });
+                      },
+                      child: Text(buttonName),
+                    ),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        foregroundColor: Colors.orange,
+                        backgroundColor: Colors.black,
                       ),
-                    ],
-                  ),
-                )
-              : Image.asset('images/bali.jpg'),
-        ),
-        bottomNavigationBar: BottomNavigationBar(
-          items: const [
-            BottomNavigationBarItem(
-              label: 'Home',
-              icon: Icon(
-                Icons.home,
-                color: Color.fromARGB(255, 67, 30, 233),
-                size: 24.0,
-                semanticLabel: 'Text to announce in accessibility modes',
+                      onPressed: () {
+                        Navigator.of(context).push(
+                          MaterialPageRoute(
+                            builder: (context) => const NextPage(),
+                          ),
+                        );
+                      },
+                      child: const Text('Next Page'),
+                    ),
+                  ],
+                ),
+              )
+            : GestureDetector(
+                onTap: () {
+                  setState(() {
+                    _isClicked = !_isClicked;
+                  });
+                },
+                child: _isClicked
+                    ? Image.asset('images/bali.jpg')
+                    : Image.network(
+                        "https://th.bing.com/th/id/OIP.K0ze806M6MD3kgAmAFQuYAHaLH?pid=ImgDet&w=204&h=306&c=7&dpr=1.5"),
               ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(
+            label: 'Home',
+            icon: Icon(
+              Icons.home,
+              color: Color.fromARGB(255, 67, 30, 233),
+              size: 24.0,
+              semanticLabel: 'Text to announce in accessibility modes',
             ),
-            BottomNavigationBarItem(
-              label: 'Settings',
-              icon: Icon(Icons.settings),
-            )
-          ],
-          currentIndex: currentIndex,
-          onTap: (int index) {
-            setState(() {
-              currentIndex = index;
-            });
-          },
-        ),
-      );
+          ),
+          BottomNavigationBarItem(
+            label: 'Settings',
+            icon: Icon(Icons.settings),
+          )
+        ],
+        currentIndex: currentIndex,
+        onTap: (int index) {
+          setState(() {
+            currentIndex = index;
+          });
+        },
+      ),
+    );
   }
 }
 
